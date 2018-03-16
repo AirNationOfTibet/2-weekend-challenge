e6d833922c9cd6d803b86d936f5b08b8d7d5a336
Tenzin Chosang <TenzinChosang@Tenzins-MacBook-Air.local>
2018-03-16 16:04:47 -0500

    set up

---
+++ server/server.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let app = express();
+
+const PORT = process.env.PORT ||  5000;
+
+app.use (express.static('server/public'));
+
+app.listen(PORT, () =>{
+    console.log('port is working on ', PORT);
+})
+
