a31f0c34c9769eaf8ba458967ba50407f19b9ef3
Tenzin Chosang <TenzinChosang@Tenzins-MacBook-Air.local>
2018-03-16 16:39:02 -0500

    filled client.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = process.env.PORT ||  5000;
 
-app.use (express.static('server/public'));
+app.use(express.static('server/public'));
 
 app.listen(PORT, () =>{
     console.log('port is working on ', PORT);
